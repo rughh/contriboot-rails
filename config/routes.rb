4a49c0b84ecf1e11b3851615c45ff43a6298e9a5
Jan Krutisch <jan@krutisch.de>
2018-02-01 17:00:39 +0100

    rails_apps_composer: extras

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
