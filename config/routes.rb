599498bb7f795e45dfa62830fa141e5cfd45248e
Jan Krutisch <jan@krutisch.de>
2018-02-01 17:00:21 +0100

    rails_apps_composer: omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signin' => 'sessions#new', :as => :signin
+  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/auth/failure' => 'sessions#failure'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
