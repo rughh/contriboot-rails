6275c6c79ef5c6d29acc1478cb0b0460cda7fce3
Jan Krutisch <jan@krutisch.de>
2018-02-01 19:19:22 +0100

    dammit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  puts "#{Rails.application.secrets.inspect}"
   provider :github, Rails.application.secrets.github_provider_key, Rails.application.secrets.github_provider_secret, scope: 'read:user'
 end
